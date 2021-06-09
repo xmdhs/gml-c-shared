43951cdaa9a8b5108d341623186bbc5c5a7bf243
xmdhs <xmdhss@gmail.com>
2021-06-09 16:38:43 +0800

    fix

--- build.bat
+++ build.bat
@@ -1,6 +1,1 @@
-cd c
-gcc -c char.c -o char.o -O3
-ar rcs libchar.a char.o
-
-cd ..
 go build -trimpath -ldflags "-w -s -linkmode \"external\" -extldflags \"-static -O3\"" -buildmode=c-shared -o cgo.dll
--- c.go
+++ c.go
@@ -8,15 +8,15 @@ import (
 	"github.com/xmdhs/gml-c-shared/c"
 )
 
-//export getchar
-func getchar(charlist **C.char, index C.longlong) *C.char {
+//export Getchar
+func Getchar(charlist **C.char, index C.longlong) *C.char {
 	c := c.Cchar{P: unsafe.Pointer(charlist)}
 	char := c.Getchar(int(index))
 	return (*C.char)(char)
 }
 
-//export freechar
-func freechar(charlist **C.char, len C.longlong) {
+//export Freechar
+func Freechar(charlist **C.char, len C.longlong) {
 	c := c.Cchar{P: unsafe.Pointer(charlist)}
 	c.Freechar(int(len))
 }
